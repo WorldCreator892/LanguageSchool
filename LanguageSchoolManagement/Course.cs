5cf1757052da16a8136c6598847033514232dbe7
Pushelle <113253250+Pushelle@users.noreply.github.com>
2022-09-30 00:44:05 +0300

    Add files via upload

---
+++ LanguageSchoolManagement/Course.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolManagement
+{
+    /// <summary>
+    /// Класс для создания и работы с курсом по заданному языку
+    /// </summary>
+    public class Course
+    {        
+        /// <summary>
+        /// Список всех доступных для изучения языков
+        /// </summary>
+        private static string[] _languageList = { "English", "French", "German", "Chinese" };
+        /// <summary>
+        /// Изучаемый на курсе язык
+        /// </summary>
+        private string _language = "Not stated";
+        /// <summary>
+        /// Количество студентов, обучающихся на курсе
+        /// </summary>
+        private int _studentAmount = 0;
+        /// <summary>
+        /// Список составленных в рамках курса групп
+        /// </summary>
+        private List<Group> _groups = new List<Group>();
+        /// <summary>
+        /// Стандартная стоимость курса
+        /// </summary>
+        private int _standartPayment = 0;
+        /// <summary>
+        /// Коэффициент повышения цены курса при выборе индивидуального обучения
+        /// </summary>
+        private int _individualCoefficient = 1;
+
+        #region Constructors
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Изучаемый на курсе язык
+        /// </summary>
+        public string Language
+        {
+            get { return _language; }
+            set { if(string.IsNullOrEmpty(value)) { _language = value; } }
+        }
+
+        /// <summary>
+        /// Количество студентов, обучающихся на курсе
+        /// </summary>
+        public int StudentAmount
+        {
+            get { return _studentAmount; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Проверяет на существование 
+        /// </summary>
+        public bool CheckLanguageExist(string CheckedLanguageName)
+        {
+            bool ans = false;
+            if(_languageList.Contains(CheckedLanguageName))
+            {
+                ans = true;
+            }
+            return ans;
+        }
+
+        /// <summary>
+        /// Добавление нового языка в список существующих
+        /// </summary>
+        public void AddNewLanguage(string AddedLanguageName)
+        {
+            if(!this.CheckLanguageExist(AddedLanguageName) & (!string.IsNullOrEmpty(AddedLanguageName)))
+            {
+                _languageList.Append(AddedLanguageName);
+            }
+        }
+        #endregion
+    }
+}
---
+++ LanguageSchoolManagement/Group.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolManagement
+{
+    /// <summary>
+    /// Класс для создания и работы с группами обучающихся (для групповых занятий)
+    /// </summary>
+    class Group
+    {
+        /// <summary>
+        /// Выбранный язык обучения
+        /// </summary>
+        private string _language = "Not stated";
+        /// <summary>
+        /// Выбранный уровень обучения
+        /// </summary>
+        private int _level = 0;
+        /// <summary>
+        /// Выбранная интенсивность обучения
+        /// </summary>
+        private int _intensity = 0;
+        /// <summary>
+        /// Количество обучающихся в группе
+        /// </summary>
+        private int _amount = 0;
+        /// <summary>
+        /// Список обучающихся
+        /// </summary>
+        private List<string> _studentNames = new List<string>();
+        /// <summary>
+        /// Уникальный идентификатор группы
+        /// </summary>
+        private int _groupID = -1;
+
+        #region Constructors
+        /// <summary>
+        /// Стандартный конструктор группы
+        /// </summary>
+        public Group(int GroupID, string GroupLanguage = "Not stated", int GroupLevel = 0, int GroupIntensity = 0)
+        {
+            _groupID = GroupID;
+            _language = GroupLanguage;
+            Level = GroupLevel;
+            Intensity = GroupIntensity;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Выбранный язык обучения
+        /// </summary>
+        public string Language
+        {
+            get { return _language; }
+            set { if (!string.IsNullOrEmpty(value)) { _language = value; } }
+        }
+        /// <summary>
+        /// Выбранный уровень обучения
+        /// </summary>
+        public int Level
+        {
+            get { return _level; }
+            set { if(value >= _level & value >=0 & value < 3) { _level = value; } }
+        }
+        /// <summary>
+        /// Выбранная интенсивность обучения
+        /// </summary>
+        public int Intensity
+        {
+            get { return _intensity; }
+            set { if (value >= 0 & value < 3) { _intensity = value; } }
+        }
+        /// <summary>
+        /// Количество обучающихся в группе
+        /// </summary>
+        public int Amount
+        {
+            get { return _amount; }
+            set { if (value >= 5 & value <= 10) { _intensity = value; } }
+        }
+        /// <summary>
+        /// Список обучающихся 
+        /// </summary>
+        public List<string> StudentNames
+        {
+            get { return _studentNames; }
+            set
+            {
+                bool NoEmptyOrNull = true;
+                foreach(string str in value)
+                {
+                    if(string.IsNullOrEmpty(str))
+                    {
+                        NoEmptyOrNull = false;
+                        break;
+                    }
+                }
+                if(NoEmptyOrNull)
+                {
+                    _studentNames = value;
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Добавление обучающегося в группу
+        /// </summary>
+        public void AddStudent(Application AcceptedApplication)
+        {
+            StudentNames.Add(AcceptedApplication.Surname);
+            _amount++;
+        }
+        #endregion
+    }
+}
